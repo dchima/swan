a7206d98246bfccd28d820b5f4079baa23174285
dchima <dcnnad@gmail.com>
2020-04-12 18:23:56 +0100

    :star: font fix

--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -86,9 +86,6 @@ const GlobalStyle = createGlobalStyle`
 
   body {
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
-      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
-      sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     background-color: ${({ theme }) => theme.body};
