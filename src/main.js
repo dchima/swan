b673d37b2be7121888c078554cc2fc5f95f7049d
dchima <dcnnad@gmail.com>
2020-04-12 20:14:05 +0100

    :star:

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { lightTheme, darkTheme } from 'utils/theme';
 import { DarkMode } from 'utils';
@@ -8,10 +8,6 @@ import {
   ErrorPage,
 } from 'pages';
 
-const Hide = styled.div`
-  font-family: 'Made Soulmaze Brush';
-  display: hidden;
-`;
 const Main = () => {
   const [theme, toggleTheme, componentMounted] = DarkMode();
   const themeMode = theme === 'light' ? lightTheme : darkTheme;
@@ -20,7 +16,6 @@ const Main = () => {
   }
   return (
       <ThemeProvider theme={themeMode}>
-        <Hide />
         <Switch>
           <Route exact path='/' render={() => <App theme={theme} toggleTheme={toggleTheme}/>} />
           <Route exact path='/404' render={() => <ErrorPage theme={theme} toggleTheme={toggleTheme}/>} />
