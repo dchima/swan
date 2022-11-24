6b63a333f9242153df947da73a36a91cbbf7c733
Daniel Chima <dcnnad@gmail.com>
2022-11-24 20:34:24 +0100

    edit content

--- src/config/content.js
+++ src/config/content.js
@@ -2,14 +2,14 @@ const content = {
   siteUrl: 'https://template.com',
   bodyText:
     `👋🏾 <br/>I’m a Full Stack Developer, Writer and Product Designer.
-    I currently <a href='/projects'>build things</a> for the internet @ <a href='https://helicarrier.studio/' target='_blank'>Helicarrier</a>.
+    I currently build things for the internet.
     
     I am a <a href='https://www.goodreads.com/review/list/112872457-chima-daniel' target='_blank'>bibliophile,</a> 
     <a href='https://open.spotify.com/user/xyggj57mzmnmx51g1qkit74bo?si=1kgEEoxYTneS2iLN53AVEg' target='_blank'>melophile,</a>
     <a href='https://letterboxd.com/dxnhima/films/' target='_blank'>cinephile,</a> and your everyday 
     <a href='https://myanimelist.net/animelist/dxnchima' target='_blank'>otaku.</a> <br/><br/>
     🌟<br/>
-    Over the years, I’ve curated a collection of <a href='/bestof'>great works</a> from all across the web. These are, in my opinion, the best of the internet. I also collect <a href='https://www.notion.so/78df0ecf0cea473a84eb7f99f34c7470?v=b4a56d75a1a545b688b2ef45242d1665' target='_blank'>art.</a>
+    Over the years, I’ve curated a collection of <a href='https://dxnchima.notion.site/Chima-s-Favorite-Articles-Content-78a8cebc34204d0bb4abf198d855d300'>great works</a> from all across the web. These are, in my opinion, the best of the internet. I also collect <a href='https://www.notion.so/78df0ecf0cea473a84eb7f99f34c7470?v=b4a56d75a1a545b688b2ef45242d1665' target='_blank'>art.</a>
     <br /><br /> 📄🖋<br/><a href='/collection'>blog,</a> <a href='https://medium.com/@chimaosx' target='_blank'>medium,</a>
     <a href='https://dev.to/dxnchima' target='_blank'>dev.to,</a> and <a href='https://desolationweb.substack.com/' target='_blank'>substack.</a>`,
 };
